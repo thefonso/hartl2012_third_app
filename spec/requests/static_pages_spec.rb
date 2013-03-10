9638bac706ac7fc565cb5418826451960138f690
thefonso <alephmage@gmail.com>
2013-03-10 12:10:07 -0500

    added a contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
@@ -15,7 +15,7 @@ describe "Static Pages" do
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
@@ -27,7 +27,7 @@ describe "Static Pages" do
   end
  
   describe "About page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About')
     end
@@ -38,4 +38,15 @@ describe "Static Pages" do
     end
   end
 
+  describe "Contact page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Ruby on Rails Sample App | Contact Us")
+    end
+  end
 end
